fbacd35f29e3437676eac240107aca80f5b82dae
nda666 <adhabakhtiar@gmail.com>
2022-07-02 18:04:48 +0700

    doran-laravel-boilerplate

---
+++ README.md
@@ -0,0 +1,12 @@
+# Doran Stack Boilerplate
+
+A backend boilerplate for PT. Doran Sukses Indonesia
+
+## How to
+
+## Dev Depency used:
+
+-   [Scribe](https://scribe.knuckles.wtf/laravel/documenting) - Api Doc Generator
+-   [Phpstan](https://github.com/phpstan/phpstan) - PHP Static Analysis Tool
+-   [Pest](https://github.com/pestphp/pest) - Elegant PHP Testing Framework
+-   [PHP_CodeSniffer](https://github.com/squizlabs/PHP_CodeSniffer) - Development tool that ensures the code remains clean and consistent
---
+++ app/Http/Controllers/Api/Home.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class Home extends Controller
+{
+    /**
+     * Home
+     *
+     * This endpoint allows you to add a word to the list.
+     * It's a really useful endpoint, and you should play around
+     * with it for a bit.
+     * <aside class="notice">We mean it; you really should.😕</aside>
+     *
+     * @queryParam name string Add your name here
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        return ['message' => 'Hello' . ($request->has('name') ? ', ' . $request->get('name') : '')];
+    }
+}
